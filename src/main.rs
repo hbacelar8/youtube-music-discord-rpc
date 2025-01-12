ed15ee001c3d412de9551e68a445019a300a3e76
Henrique Bacelar <dev@bacelar.fr>
2025-01-12 20:15:11 +0100

    Fix error preventing activity changing

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,72 @@
 use discord_rpc_client::Client as DiscordClient;
-use playerctl_rust_wrapper::Playerctl;
+use playerctl_rust_wrapper::{Playerctl, TrackMetadata};
 use regex::Regex;
 use std::{thread, time};
+
+const REFRESH_RATE_S: u64 = 2;
+
+#[derive(Default)]
+struct ActivityData {
+    track_metadata: TrackMetadata,
+}
 
 fn main() {
     // Compile the regex pattern
     let re = Regex::new(r"music.youtube.com").unwrap();
 
+    // Activity struct data
+    let mut activity_data = ActivityData::default();
+    let mut has_activity = false;
+
     // Create new discord client
     let mut drpc = DiscordClient::new(1327589296177418291);
 
-    // Whether the program has ownership of current activity
-    let mut has_activity = false;
-
     // Add ready event
     drpc.on_ready(|_| {
-        println!("Ready");
+        println!("Discord connection ready");
     });
 
     // Start the client
     drpc.start();
 
     loop {
-        // Get track metadata
-        let metadata = Playerctl::metadata().unwrap();
+        if let Ok(metadata) = Playerctl::metadata() {
+            if !metadata.url.is_empty() && re.is_match(&metadata.url) {
+                if metadata.title != activity_data.track_metadata.title {
+                    has_activity = true;
+                    activity_data.track_metadata = metadata;
 
-        // Check if track comes from Youtube Music
-        if !metadata.url.is_empty() && re.is_match(&metadata.url) {
-            if !has_activity {
-                has_activity = true;
+                    let mut title = activity_data.track_metadata.title.clone();
 
-                let mut title = metadata.title.clone();
+                    if !activity_data.track_metadata.album.is_empty() {
+                        title.push_str(
+                            format!(" | {}", activity_data.track_metadata.album).as_str(),
+                        );
+                    }
 
-                if !metadata.album.is_empty() {
-                    title.push_str(format!(" | {}", metadata.album).as_str());
+                    if let Err(err) = drpc.set_activity(|a| {
+                        a.state(format!("by {}", activity_data.track_metadata.artist))
+                            .details(title)
+                    }) {
+                        eprintln!("Failed to set presence: {}", err);
+                    } else {
+                        println!(
+                            "Presence set => Title: {} | Album: {} | Artist: {}",
+                            activity_data.track_metadata.title,
+                            activity_data.track_metadata.album,
+                            activity_data.track_metadata.artist
+                        );
+                    }
                 }
-
-                if let Err(err) =
-                    drpc.set_activity(|a| a.state(format!("by {}", metadata.artist)).details(title))
-                {
-                    eprintln!("Failed to set presence: {}", err);
-                }
+            } else if has_activity {
+                println!("Player no longer detected, clearing activity");
+                activity_data = ActivityData::default();
+                has_activity = false;
+                drpc.clear_activity().unwrap();
             }
-        } else if has_activity {
-            has_activity = false;
-            drpc.clear_activity().unwrap();
         }
 
         // Wait 3 seconds
-        thread::sleep(time::Duration::from_secs(3));
+        thread::sleep(time::Duration::from_secs(REFRESH_RATE_S));
     }
 }
